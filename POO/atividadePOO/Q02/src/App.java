6864f4e18bb51fc22fcc20ad49230ce3fc614e52
PicolasCage243 <142632758+PicolasCage243@users.noreply.github.com>
2023-11-04 20:54:47 -0300

    cheque especial by marcony

--- POO/atividadePOO/Q02/src/App.java
+++ POO/atividadePOO/Q02/src/App.java
@@ -8,7 +8,7 @@ public class App {
 
         while(menu == 1){
 
-        System.out.println("Bem-vindo ao sistema do banco, qual ação deseja realizar? \n1)Depositar \n2)Sacar \n3) Ver saldo \n4)Sair");
+        System.out.println("Bem-vindo ao sistema do banco, qual ação deseja realizar? \n1) Depositar \n2) Sacar \n3) Ver saldo \n4) Sair");
         int digito = scanner.nextInt();
         if(digito != 1 && digito != 2 && digito != 3 && digito != 4 ){
         System.out.println("Não há esta opção no menu!\n");
--- POO/atividadePOO/Q02/src/Conta.java
+++ POO/atividadePOO/Q02/src/Conta.java
@@ -4,6 +4,7 @@ public class Conta {
     Scanner acao = new Scanner(System.in);
 
     double saldo = 0, deposito, saque;
+    double limite = 2000;
 
     public double depositar(){
         System.out.println("Qual o valor do depósito? \n");
@@ -16,15 +17,25 @@ public class Conta {
     public double sacar(){
         System.out.println("Qual o valor do saque?\n");
         saque = acao.nextDouble();
-        double taxa = (saque*0.005);
+         double taxa = (saque*0.005);
+        if((saldo+limite)>=(saque+taxa)){
         saldo -= saque + taxa;
          System.out.println("Saque realizado no valor de: R$"+saque);
          return saque;
+        }
+        else{
+            System.out.println("Saldo insuficiente!");
+            return taxa = 0;
+        }
     }
 
-    public double versaldo(){
+    public void versaldo(){
+        if(saldo<0){
+            System.out.println("ATENÇÃO!\nVocê está utilizando do cheque especial! \nSeu saldo é de: R$"+ saldo);
+        }
+        else{
         System.out.println("Seu saldo é de: R$"+saldo);
-        return saldo;
+        }
     }
 
 }
