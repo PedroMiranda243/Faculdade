c3e323198a0112029a0ac3b4aafd158e3d403119
Pedro Miranda <142632758+PedroMiranda243@users.noreply.github.com>
2023-11-10 12:34:17 -0300

    calculadora

---
+++ WebCoding/Calculadora/script.js
@@ -0,0 +1,23 @@
+function inserir(valor){
+var x = document.getElementById('tela').innerHTML
+document.getElementById('tela').innerHTML = x + valor
+}
+
+function limpar(){
+    document.getElementById('tela').innerHTML=""
+}
+
+function voltar(){
+    var x = document.getElementById('tela').innerHTML
+    document.getElementById('tela').innerHTML = x.substring(0, x.length -1)
+}
+
+function calcular(){
+    var x =  document.getElementById('tela').innerHTML
+    if(x){
+        document.getElementById('tela').innerHTML = eval(x)
+    }
+    else{
+        document.getElementById('tela').innerHTML = "vazio.."
+    }
+}
